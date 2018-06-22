7e06c4bb7fd9b81aee49f9caa36919dd4880f81d
unknown <egemen.kaba@gmail.com>
2018-06-22 17:31:35 +0200

    added action

---
+++ src/action.ts
@@ -0,0 +1,9 @@
+export enum Action {
+    CAMPFIRE = "Campfire",
+    PLANE = "Plane",
+    IDLING = "Idling",
+    STARGAZING = "Stargazing",
+    SANDCASTLES = "Sandcastles",
+    TINKERING = "Tinkering",
+    EMPTY = ""
+}
